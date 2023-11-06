f21d4507194da7bff48671b20f63aa58bf213030
Jiří Mancl <jiri.mancl@technodat.cz>
2023-11-06 11:47:18 +0100

    Generic repository added

---
+++ LeaveManagement.Web/Repositories/GenericRepository.cs
@@ -0,0 +1,49 @@
+﻿using LeaveManagement.Web.Contracts;
+using LeaveManagement.Web.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeaveManagement.Web.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext context;
+
+        public GenericRepository(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public async Task<T> AddAsync(T entity)
+        {
+            await context.AddAsync(entity);
+            await context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await GetAsync(id);
+            if (entity is not null)
+            {
+                context.Set<T>().Remove(entity);
+            }
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<bool> Exists(int id)
+        {
+            var entity = await GetAsync(id);
+            return entity is not null;
+        }
+
+        public async Task<List<T>> GetAllAsync() => await context.Set<T>().ToListAsync();
+
+        public async Task<T> GetAsync(int id) => await context.Set<T>().FindAsync(id);
+
+        public async Task UpdateAsync(T entity)
+        {
+            //context.Entry(entity).State = EntityState.Modified; Another option
+            context.Update(entity);
+            await context.SaveChangesAsync();
+        }
+    }
+}
